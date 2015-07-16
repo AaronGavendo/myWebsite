20c985b3c8aff1650fd6ad6e3bddf8404553646d
AaronGavendo <agavendo@hotmail.com>
2015-07-15 23:46:41 -0400

    10: Let headers, footers and side bar be populated with JS

---
+++ js/aaron.js
@@ -0,0 +1,14 @@
+$(document).ready(function (){
+	console.log("ready function in aaron.js runs");
+	document.getElementById("header").innerHTML = "Aaron Gavendo";
+	document.getElementById("footer").innerHTML = "Aaron Gavendo 2015";
+
+	document.getElementById("index").innerHTML = "Home";
+	document.getElementById("news").innerHTML = "News";
+	document.getElementById("blogs").innerHTML = "Blogs";
+	document.getElementById("gameR").innerHTML = "Game Reviews";
+	document.getElementById("sampleW").innerHTML = "Sample Work";
+	document.getElementById("games").innerHTML = "Games";
+
+
+});
