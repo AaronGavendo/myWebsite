a6bed514276b81fd6be9d01d0c2068367a2504a2
AaronGavendo <agavendo@hotmail.com>
2016-03-25 17:31:39 -0400

    #28 Add thumbnail to pages built off the template. Build new js for pages not built off the template to add thumbnail later.

---
+++ js/aaron-notemplate.js
@@ -0,0 +1,6 @@
+$(document).ready(function (){
+	console.log("ready function in aaron.js runs");
+    $('head').append('<link rel="icon" href="Pictures/icon.png">');
+    $('head').append('<link rel="icon" href="../Pictures/icon.png">');
+    $('head').append('<link rel="icon" href="../../Pictures/icon.png">');
+});
--- js/aaron.js
+++ js/aaron.js
@@ -2,6 +2,9 @@ $(document).ready(function (){
 	console.log("ready function in aaron.js runs");
 	document.getElementById("header").innerHTML = "Aaron Gavendo";
 	document.getElementById("footer").innerHTML = "Aaron Gavendo 2016";
+    $('head').append('<link rel="icon" href="Pictures/icon.png">');
+    $('head').append('<link rel="icon" href="../Pictures/icon.png">');
+    $('head').append('<link rel="icon" href="../../Pictures/icon.png">');
 
 	document.getElementById("index").innerHTML = "Home";
 	document.getElementById("news").innerHTML = "News";
@@ -9,6 +12,8 @@ $(document).ready(function (){
 	document.getElementById("gameR").innerHTML = "Game Reviews";
 	document.getElementById("sampleW").innerHTML = "Sample Work";
 	document.getElementById("games").innerHTML = "Games";
+
+
 
 
 });
